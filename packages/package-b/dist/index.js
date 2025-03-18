222f2d4202452a49ead3565da2a9617a343cbfe3
bianxk <523688711@qq.com>
2025-03-18 10:44:20 +0800

    feat: init

---
+++ packages/package-b/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.welcomeUser = welcomeUser;
+const package_a_1 = require("@my-scope/package-a");
+function welcomeUser(name) {
+    const greeting = (0, package_a_1.greet)(name);
+    return `${greeting} Welcome to our platform.`;
+}
