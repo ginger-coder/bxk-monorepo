51d542082b41edd50c7dddf7f1e87b1a15f41265
bianxk <523688711@qq.com>
2025-03-20 15:42:41 +0800

    feat

--- packages/package-a/dist/index.js
+++ packages/package-a/dist/index.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.greet = void 0;
 function greet(name) {
-    return `Hello, ${name}!`;
+    return `Hello, ${name}! --by box`;
 }
 exports.greet = greet;
