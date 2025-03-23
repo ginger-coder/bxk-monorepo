3ad6069078e03ce6e48f4451e78e614fa52b0c11
Bianxk <bxk0129@163.com>
2025-03-24 00:55:53 +0800

    feat

--- packages/package-b/dist/index.js
+++ packages/package-b/dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.welcomeUser = void 0;
-const package_a_1 = require("@my-scope/package-a");
+const package_a_1 = require("@bianxk/package-a");
 function welcomeUser(name) {
     const greeting = (0, package_a_1.greet)(name);
     return `${greeting} Welcome to our platform. --by bianxk --`;
--- packages/package-b/src/index.ts
+++ packages/package-b/src/index.ts
@@ -1,4 +1,4 @@
-import { greet } from "@my-scope/package-a";
+import { greet } from "@bianxk/package-a";
 export function welcomeUser(name: string): string {
   const greeting = greet(name);
   return `${greeting} Welcome to our platform. --by bianxk --`;
